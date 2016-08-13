b572400fa83f7cffbbc7ac8d682b6e09ca400362
Unvoxel <jb@unvoxel.com>
2016-08-13 21:47:48 +1000

    added unvoxeled function"

--- fromSurfaceToLine.py
+++ fromSurfaceToLine.py
@@ -1,11 +1,10 @@
-##### !!!!!! Need to deal with closed curve
-##### should deal with startTuplePoint instead of startPoint
-
-
 import rhinoscriptsyntax as rs
 import Rhino
 import scriptcontext
 import copy
+import random
+import datetime
+import math
 
 
 
@@ -53,19 +52,11 @@ def endMiddleAngleCurve(curve, line):
 
 # Curve deviation is the maximum distance between two curves
 def firstSegmenter(curve, angleMax,deviationMax, width, segmentList):
-    #Analyse of the curvature
-    if rs.CurveStartPoint(curve) == rs.CurveEndPoint(curve): # If it is a closed curve
-        (curve1, curve2) = rs.SplitCurve(curve, rs.CurveClosestPoint(curve, rs.CurveMidPoint(curve)))
-        rs.DeleteObjects(curve)
-        segmentList = firstSegmenter(curve1, angleMax, deviationMax, width, segmentList)
-        segmentList = firstSegmenter(curve2, angleMax, deviationMax, width, segmentList)
-    else:
-        draftLine = rs.AddLine(rs.CurveStartPoint(curve), rs.CurveEndPoint(curve))
+    draftLine = rs.AddLine(rs.CurveStartPoint(curve), rs.CurveEndPoint(curve))
     if rs.CurveLength(curve) > width*2: # Criteria D
         curveDeviation = rs.CurveDeviation(curve,draftLine)
         if curveDeviation: #in case curveDeviation doesn't work, we use the distance midpoint of curve and midpoint of draftLine
             if curveDeviation[2] > deviationMax or abs(endMiddleAngleCurve(curve, draftLine)) > angleMax or abs(startMiddleAngleCurve(curve, draftLine)) > angleMax:
-                #rs.DeleteObject(draftLine)
                 (curve1, curve2) = rs.SplitCurve(curve, rs.CurveClosestPoint(curve, rs.CurveMidPoint(curve)))
                 rs.DeleteObjects([curve, draftLine])
                 segmentList = firstSegmenter(curve1, angleMax, deviationMax, width, segmentList)
@@ -76,7 +67,6 @@ def firstSegmenter(curve, angleMax,deviationMax, width, segmentList):
                 rs.DeleteObjects([curve, draftLine])
                 return segmentList
         elif rs.Distance(rs.CurveMidPoint(curve), rs.CurveMidPoint(draftLine)) > deviationMax or abs(endMiddleAngleCurve(curve, draftLine)) > angleMax or abs(startMiddleAngleCurve(curve, draftLine)) > angleMax:
-            #rs.DeleteObject(draftLine)
             (curve1, curve2) = rs.SplitCurve(curve, rs.CurveClosestPoint(curve, rs.CurveMidPoint(curve)))
             rs.DeleteObjects([curve, draftLine])
             segmentList = firstSegmenter(curve1, angleMax, deviationMax, width, segmentList)
@@ -95,8 +85,13 @@ def firstSegmenter(curve, angleMax,deviationMax, width, segmentList):
 
 def segmenter(curve, angleMax, deviationMax, width):
     segmentList = [rs.CurveStartPoint(curve)]
-    # curveCopy = rs.CopyObject(curve) (uncomment is you want to display it)
-    segmentList = firstSegmenter(curve, angleMax,deviationMax, width, segmentList)
+    if rs.CurveStartPoint(curve) == rs.CurveEndPoint(curve): # If it is a closed curve
+        (curve1, curve2) = rs.SplitCurve(curve, rs.CurveClosestPoint(curve, rs.CurveMidPoint(curve)))
+        rs.DeleteObjects(curve)
+        segmentList = firstSegmenter(curve1, angleMax, deviationMax, width, segmentList)
+        segmentList = firstSegmenter(curve2, angleMax, deviationMax, width, segmentList)
+    else:
+        segmentList = firstSegmenter(curve, angleMax, deviationMax, width, segmentList)
     return segmentList
 
 
@@ -135,8 +130,8 @@ def fromSurfaceToLine(layerHeight, srf):
     numberOfLayers = int((height - 0.5) / layerHeight) + 1
     curveList = []
     for i in range(0, numberOfLayers):
-        corner1 = str(BB[0][0]) + "," + str(BB[0][1]) + "," + str(BB[0][2] + i*layerHeight + 0.5 ) # +0.5 because it bugs when we interesect right on the bottom edge
-        corner2 = str(BB[2][0]) + "," + str(BB[2][1]) + "," + str(BB[2][2] + i*layerHeight + 0.5 )
+        corner1 = str(BB[0][0] - 1) + "," + str(BB[0][1] - 1) + "," + str(BB[0][2] + i*layerHeight + 0.5 ) # +0.5 because it bugs when we interesect right on the bottom edge
+        corner2 = str(BB[2][0] + 1) + "," + str(BB[2][1] + 1) + "," + str(BB[2][2] + i*layerHeight + 0.5 ) # + 1 and  - 1 to make sure that section section plane is bigger than object
         rs.Command("-plane " + corner1 + " " + corner2 + " " )
         layer = rs.LastCreatedObjects()
         rs.UnselectAllObjects()
@@ -147,6 +142,19 @@ def fromSurfaceToLine(layerHeight, srf):
         curveList.append(rs.LastCreatedObjects()[0])
     return (curveList)
 
+
+# Change the startPoint randomly for all closed curves
+def startPointChanger(curveList):
+    for layer in range(0, len(curveList)):
+        for curve in range(0, len(curveList[layer])):
+            if rs.IsCurveClosed(curveList[layer][curve]):
+                parameter = random.random()*rs.CurveLength(curveList[layer][curve])
+                rs.CurveSeam(curveList[layer][curve], parameter)
+#result = []
+#curveList = []
+#curveList.append(rs.GetObject())
+#result.append(curveList)
+#print startPointChanger(result)
 
 # returns a sorted list according to the distance from 1 point
 # This function has to be used from the end point of the start curve to the start point of the end curve
@@ -243,6 +251,7 @@ def sortPointsWithinLayer(startPoint, curveList, sortedCurveList, newTuplePointL
             pass
     
     if len(sortedCurveList) == len(curveList): # this was the last iteration, endPoint is defined here
+        endPoint = newTuplePointList[-1][1]
         return [newTuplePointList, newTuplePointList[-1][1], sortedCurveList]
     else:
         pointList = fromCurveListToPointList(list(set(curveList) - set(sortedCurveList))) # List of remaining points
@@ -326,8 +335,6 @@ def fullSortedPointList(startPoint, curveList, objectTuplePointList, objectCurve
     [layerTuplePointList, endPoint, sortedCurveList] = sortPointsWithinLayer(startPoint, curveList[layerNumber], [], [])
     objectTuplePointList[layerNumber] = layerTuplePointList
     objectCurveList.append(sortedCurveList)
-    # Define and append the last tuple
-    objectTuplePointList[layerNumber].append([ endPoint, otherEndPoint(endPoint, sortedCurveList[-1]) ])
     # Determine the next startPoint / only if there is a next layer
     if layerNumber == numberOfLayers - 1:
         return objectTuplePointList, objectCurveList, curveList, layerNumber, numberOfLayers
@@ -350,33 +357,6 @@ def connectionDisplay(objectTuplePointList):
             drawLine(objectTuplePointList[layer][-1][1], objectTuplePointList[layer + 1][0][0])
 
 
-def multipleSrf(layerHeight):
-    rs.UnselectAllObjects()
-    srf = []
-    i = 0
-    listOfNumberOfLayers = [0]
-    curveList = []
-    newCurveList = []
-    layerCurveList = []
-    
-    srf = selectSurfaces()
-    
-    numberOfSurfaces = len(srf)
-    for p in range(0, numberOfSurfaces):
-        curveList.append(fromSurfaceToLine(layerHeight, srf[p]))
-    
-    #re-organization of the list of curve
-    for j in range( 0, max(len(curveList[k]) for k in range(0, numberOfSurfaces)) ):
-        for i in range(0, numberOfSurfaces):
-            try:
-                newCurveList.append(curveList[i][j])
-            except:
-                pass
-        layerCurveList.append(newCurveList)
-        newCurveList = []
-    return layerCurveList
-
-
 def reorganisation(segmentList, tuplePoint):
     newCurvePointList = []
     if segmentList[0] == tuplePoint[0] and segmentList[-1] == tuplePoint[-1]:
@@ -390,11 +370,83 @@ def reorganisation(segmentList, tuplePoint):
         return segmentList
 
 
+def multipleSrf(layerHeight):
+    rs.UnselectAllObjects()
+    srf = []
+    i = 0
+    listOfNumberOfLayers = [0]
+    curveList = []
+    newCurveList = []
+    layerCurveList = []
+    srf = selectSurfaces()
+    numberOfSurfaces = len(srf)
+    for p in range(0, numberOfSurfaces):
+        curveList.append(fromSurfaceToLine(layerHeight, srf[p]))
+    #re-organization of the list of curve
+    for j in range( 0, max(len(curveList[k]) for k in range(0, numberOfSurfaces)) ):
+        for i in range(0, numberOfSurfaces):
+            try:
+                newCurveList.append(curveList[i][j])
+            except:
+                pass
+        layerCurveList.append(newCurveList)
+        newCurveList = []
+    return layerCurveList
+
+
+def unvoxelGCode(voxeledGCode):
+    unvoxeledGCode = []
+    printTime = datetime.datetime.now()
+    firstLine = ';GCode generated by Unvoxel - %s \n\n' %printTime
+    unvoxeledGCode.append(firstLine)
+    unvoxeledGCode.append('G28 ; home all axes\n')
+    unvoxeledGCode.append('G1 Z5 F5000 ; lift nozzle\n')
+    unvoxeledGCode.append('G21 ; set units to millimeters\n')
+    unvoxeledGCode.append('M82 ; use absolute distances for extrusion\n')
+    unvoxeledGCode.append('G92 E0\n')
+    
+    for i in range(0, len(voxeledGCode)):
+        newLine = 'G' + str(voxeledGCode[i][0])
+        newLine += ' Z' + str(round(voxeledGCode[i][1], 3))
+        newLine += ' X' + str(round(voxeledGCode[i][2], 3))
+        newLine += ' Y' + str(round(voxeledGCode[i][3], 3))
+        newLine += ' E' + str(round(voxeledGCode[i][4], 5))
+        newLine += ' F' + str(voxeledGCode[i][5])
+        unvoxeledGCode.append ( newLine + '\n' )
+    unvoxeledGCode.append('M84     ; disable motors')
+    return unvoxeledGCode
+
+def writeFile( fullFilename, newContent ):
+    with open(fullFilename, 'w') as f:
+        for i in range(0, len(newContent)):
+            f.write(newContent[i])
+    f.close
+
+#Theory
+def fromGeotoNr(layerHeight, width, length): #get the number of rotation of the auger for 1 length of extrusion for a given geometry (width and thickness)
+    chamberVolume = 817.7 #Chamber volume of the Auger in mm3
+    numberOfRotations = length * layerHeight * width / chamberVolume
+    return numberOfRotations
+
+def fromNrtoE(numberOfRotations): #get the E (in mm) for a given number of rotations
+    E = numberOfRotations * 19.8 #Empirical
+    return E
+
+def ECalculation(layerHeight, width, XBefore, XAfter, YBefore, YAfter):
+    length = math.sqrt((XBefore - XAfter)**2 + (YBefore - YAfter)**2)
+    numberOfRotations = fromGeotoNr(layerHeight, width, length)
+    return fromNrtoE(numberOfRotations)
+
+
 def main():
     # Input
     layerHeight = float(input("Layer Height?"))
-    layerWidth = float(input("Layer Width?"))
+    width = float(input("Layer Width?"))
+    speed = float(input("Speed (in mm/s)?"))
     curveList = multipleSrf(layerHeight)
+    
+    # Reorganize the startPoint for closed curves
+    startPointChanger(curveList)
     
     # Initialization
     angleMax = 5
@@ -406,46 +458,47 @@ def main():
     objectCurveList = []
     for i in range(0, numberOfLayers):
         objectTuplePointList.append([])
-    voxeledStep = []
     voxeledGCode = []
     
     # Algorithm
     result = fullSortedPointList(startPoint, curveList, objectTuplePointList, objectCurveList, -1, numberOfLayers)
     objectTuplePointList = result[0]
     objectCurveList = result[1]
-    connectionDisplay(objectTuplePointList)
+    connectionDisplay(objectTuplePointList)    
     
     # Segmentation + voxeled GCode
     for layerNumber in range(0, len(curveList)): #For each layer of 1 object
         for curveNumber in range(0, len(objectCurveList[layerNumber])): # For each curve of 1 layer
-            segmentList = segmenter(objectCurveList[layerNumber][curveNumber], angleMax, deviationMax, layerWidth)
-            segmenterResultDisplay(segmentList)
+            segmentList = segmenter(objectCurveList[layerNumber][curveNumber], angleMax, deviationMax, width)
+            #segmenterResultDisplay(segmentList)
             segmentList = reorganisation(segmentList, objectTuplePointList[layerNumber][curveNumber])
-            for point in range(0, len(segmentList) - 1): # For each segment of 1 curve
-                voxeledStep.append("G1")
-                a = segmentList[point]
-                voxeledStep.append(segmentList[point][2])
-                voxeledStep.append(segmentList[point][0])
-                voxeledStep.append(segmentList[point][1])
-                voxeledStep.append(999999999)
-                voxeledStep.append(999999999)
-                voxeledGCode.append(voxeledStep)
-            voxeledStep.append("G1")
-            voxeledStep.append(segmentList[point][2])
-            voxeledStep.append(segmentList[point][0])
-            voxeledStep.append(segmentList[point][1])
+            numberOfSegments = len(segmentList)
+            
+            voxeledStep = []
+            voxeledStep.append("1")
+            voxeledStep.append(segmentList[0][2])
+            voxeledStep.append(segmentList[0][0])
+            voxeledStep.append(segmentList[0][1])
             voxeledStep.append(0)
-            voxeledStep.append(999999999)
+            voxeledStep.append(speed * 60)
             voxeledGCode.append(voxeledStep)
+            
+            for point in range(1, numberOfSegments): # For each segment of 1 curve
+                voxeledStep = []
+                voxeledStep.append("1") #G
+                a = segmentList[point]
+                voxeledStep.append(segmentList[point][2]) #Z
+                voxeledStep.append(segmentList[point][0]) #X
+                voxeledStep.append(segmentList[point][1]) #Y
+                voxeledStep.append( ECalculation(layerHeight, width, segmentList[point][0], segmentList[point-1][0], segmentList[point][1], segmentList[point-1][1]) ) #E
+                voxeledStep.append(speed * 60) #F in mm per min
+                voxeledGCode.append(voxeledStep)
     
     # Unvoxeled GCode
+    fileContent = unvoxelGCode(voxeledGCode)
+    filename = input("Filename?")
+    fullFilename = "C:\\Users\\JB\\Documents\\1 - Technical\\1 - Design\\3 - GCode\\1 - Import folder\\1 - GCode from Unvoxel\\" + filename.__name__ + ".txt"
+    writeFile( fullFilename, fileContent )
 
 
 main()
-
-def checkSamePoint(voxeledGCode):
-    for i in range(voxeledGCode):
-        for j in range(voxeledGCode):
-            if voxeledGCode[i][2] == voxeledGCode[j][2]:
-                return 1
-    return 0
